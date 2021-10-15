baf456a5db0a51cbdd7c841a275c075679b75905
bsbarbara17 <barbarabs1703@gmail.com>
2021-10-15 15:23:34 -0300

    bora galera

--- react/site/src/services/api.js
+++ react/site/src/services/api.js
@@ -4,11 +4,105 @@ const api = axios.create({
 })
 
 export default class Api {
-    async Agendar(idfuncionario, idcliente, idservico, data, situacao) {
-        let r = await api.post('/agendamento', {})
+    async ListarCliente() {
+        let r = await api.get('/cliente', {})
     }
 
-    async Remover(idfuncionario, idcliente, idservico, data, situacao) {
+    async InserirCliente(nome, email, senha, telefone) {
+        let r = await api.post('/cliente', { nome, email, senha, telefone});
+        return r.data;
+    }
+
+    async AlterarCliente(id, nome, email, senha, imagem, telefone) {
+        let r = await api.put('/cliente' + id, { nome, email, senha, imagem, telefone });
+        return r.data;
+    }
+
+    async RemoverCliente(id) {
+        let r = await api.delete('/cliente/' + id);
+        return r.data;
+    }
+
+
+
+    async ListarFuncionario() {
+        let r = await api.get('/funcionario', {})
+    }
+
+    async InserirFuncionario(nome, cargo, email, senha, telefone) {
+        let r = await api.post('/funcionario', { nome, cargo, email, senha, telefone });
+        return r.data;
+    }
+
+    async AlterarFuncionario(id, nome, cargo, email, senha, telefone) {
+        let r = await api.put('/funcionario' + id, { nome, cargo, email, senha, telefone });
+        return r.data;
+    }
+
+    async RemoverFuncionario(id) {
+        let r = await api.delete('/funcionario/' + id);
+        return r.data;
+    }
+
+
+
+    async ListarServicos() {
+        let r = await api.get('/servicos', {})
+    }
+
+    async InserirServicos(tipo, nome, descricao, valor) {
+        let r = await api.post('/servicos', { tipo, nome, descricao, valor });
+        return r.data;
+    }
+
+    async AlterarServicos(id, tipo, nome, descricao, valor) {
+        let r = await api.put('/servicos' + id, { tipo, nome, descricao, valor });
+        return r.data;
+    }
+
+    async RemoverServicos(id) {
+        let r = await api.delete('/servicos/' + id);
+        return r.data;
+    }
+
+
+
+    async ListarServicosImg() {
+        let r = await api.get('/servicoimg', {})
+    }
+
+    async InserirServicosImg(idservico, imgservico) {
+        let r = await api.post('/servicoimg', { idservico, imgservico });
+        return r.data;
+    }
+
+    async AlterarServicosimg(id, idservico, imgservico) {
+        let r = await api.put('/servicos' + id, { idservico, imgservico });
+        return r.data;
+    }
+
+    async RemoverServicosImg(id) {
+        let r = await api.delete('/servicoimg/' + id);
+        return r.data;
+    }
+    
+    
+    
+    async ListarAgendamento() {
+        let r = await api.get('/agendamento', {})
+    }
+
+    async InserirAgendamento(funcionario, cliente, servico, agendamento, situacao) {
+        let r = await api.post('/agendamento', { funcionario, cliente, servico, agendamento, situacao });
+        return r.data;
+    }
+
+    async AlterarAgendamento(id, funcionario, servico, agendamento) {
+        let r = await api.put('/agendamento' + id, { funcionario, servico, agendamento });
+        return r.data;
+    }
+
+    async RemoverAgendamento(id) {
         let r = await api.delete('/agendamento/' + id);
         return r.data;
     }
