bdffce0902c57c3a79dc41987288ec6518ed2b02
Gustalacoste <gustavosg2019e@gmail.com>
2021-10-31 11:22:22 -0300

    OK

--- react/site/src/pages/perfilFuncionario/index.js
+++ react/site/src/pages/perfilFuncionario/index.js
@@ -123,15 +123,15 @@ export default function MeuPerfil(){
                     
                     
                     <div className="dadosperfil">
-                        <div className="dados">Nome do Cliente  </div>
+                        <div className="dados">Nome do Funcionário  </div>
                     </div>
                 </div>
         
                 <div className="faixa2"> 
                     <div className="textaviso">Avisos</div>
                     <div className="avisos">
-                    <div className="avisotext">(11:21:22) Carlos desmarcou o serviço do dia xxxx as xxx horas</div>
-                    <div className="avisotext">(11:21:22) Carlos Remarcou o serviço do dia xxxx as xxx para o dia xxxx as xxxx horas</div>
+                    <div className="avisotext">(11:21:22) Bruno desmarcou o serviço do dia xxxx as xxx horas</div>
+                    <div className="avisotext">(11:21:22) Bruno Remarcou o serviço do dia xxxx as xxx para o dia xxxx as xxxx horas</div>
                     <div className="avisotext"><br></br></div>
                     <div className="avisotext"><br></br></div>
                     <div className="avisotext"><br></br></div>
@@ -149,9 +149,7 @@ export default function MeuPerfil(){
                                 <th> Id </th>
                                 <th> Cliente </th>
                                 <th> Serviço </th>
-                                <th> Profissional </th>
                                 <th> Horario Agenda </th>
-                                <th> Situacão </th>
                                 <th className="a"> </th>
                             </tr>
                         </thead>
@@ -166,9 +164,7 @@ export default function MeuPerfil(){
                                                 : item.nm_cliente} 
                                     </td>
                                     <td> {item.nm_servico} </td>
-                                    <td> {item.nm_funcionario} </td>
                                     <td> {item.dt_agendamento} </td>
-                                    <td> {item.tp_situacao} </td>
                                     <td className="acao"> <button onClick={ () => editar(item) }> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
                                     <td className="acao"> <button onClick={ () => remover(item.id_agendamento) }> <img src="/assets/images/delete.svg" alt="" /> </button> </td>
                                 </tr>
