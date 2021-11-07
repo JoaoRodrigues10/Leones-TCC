2b7cd057217a6c5d26c80b300f3896fa00f1f63f
João <joaopedr8945@gmail.com>
2021-11-06 23:58:32 -0300

    CookiesDeLoginFeito

--- react/site/src/components/cabecalho/index.js
+++ react/site/src/components/cabecalho/index.js
@@ -1,10 +1,30 @@
 
 import { Link } from 'react-router-dom'
 import { Cabecalho } from './styled'
+import Cookies from 'js-cookie'
+// import { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 
 export default function Cabecalhooo() {
-    
+    // const [logado, setLogado] = useState(false)
+    const nav = useHistory()
+
+    const logoof = () => {
+        Cookies.remove('usuario-logado')
+        nav.push('/')
+    }
+
+    // const logadoOUnao = () => {
+       
+    //     let usuarioLogado = Cookies.get('usuario-logado')
+    //     if (usuarioLogado == null) {
+    //         setLogado(false)
+    //     } else {
+    //         setLogado(true);
+    //     }
+    // }
+
     return (
             <Cabecalho>
                 <div className="logo-cab">
@@ -16,6 +36,12 @@ export default function Cabecalhooo() {
                 <Link to="/home"> <div className="opcoes"> LEO </div> </Link>     
                 </div>
                 <div>
+                    
+                        
+                            <Link to="/entrar"><button onClick={logoof}>SAIR</button> </Link>
+                        
+
+                    
                     <Link to="/meuPerfil"><div className="imagem-cab2"> <img src="/assets/images/fotousu.png" alt=""  /> </div></Link>
                 </div>
             </Cabecalho> 
